224fc2c9d7eb420f00f1aeeaa80684d1de48cf89
ant100pshnik <mypochtav@gmail.com>
2024-10-03 19:04:28 +0300

    remove redundant comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       blogger: ["BloggerSans", "sans-serif"],
-  //     },
-  //   },
-  // },
   plugins: [],
 };
