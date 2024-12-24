a37d138cda596180a687f589ae3529ebece77d66
ant100pshnik <mypochtav@gmail.com>
2024-12-24 23:45:25 +0200

    completed contacts sections

--- src/components/sections/Contacts.tsx
+++ src/components/sections/Contacts.tsx
@@ -1,6 +1,7 @@
 import Instagram from "/socials/instagram.svg";
 import Telegram from "/socials/telegram.svg";
 import LinkedIn from "/socials/linkedin.svg";
+import GMail from "/socials/gmail.svg";
 import Bratochkin from "/bratochkin.png";
 import { motion } from "framer-motion";
 import { useEffect, useRef } from "react";
@@ -8,6 +9,12 @@ import { useEffect, useRef } from "react";
 interface Social {
   logo: string;
   href: string;
+}
+
+interface BratochkinSocial {
+  logo: string;
+  href: string;
+  text: string;
 }
 
 const socials: Social[] = [
@@ -22,6 +29,29 @@ const socials: Social[] = [
   {
     logo: LinkedIn,
     href: "https://www.linkedin.com/in/oleg-br%C3%A1tochkin-229188185/",
+  },
+];
+
+const bratochkinSocials: BratochkinSocial[] = [
+  {
+    logo: Instagram,
+    href: "https://www.instagram.com/simplemaker/",
+    text: "Ask on Instagram",
+  },
+  {
+    logo: Telegram,
+    href: "https://t.me/simplemaker",
+    text: "Ask on Telegram",
+  },
+  {
+    logo: LinkedIn,
+    href: "https://www.linkedin.com/in/oleg-br%C3%A1tochkin-229188185/",
+    text: "Ask on LinkedIn",
+  },
+  {
+    logo: GMail,
+    href: "mailto:lucidmediabook@gmail.com",
+    text: "Ask on GMail",
   },
 ];
 
@@ -108,12 +138,24 @@ export const Contacts = () => {
                   src={Bratochkin}
                   alt="Oleg Bratochkin"
                 />
-                <div className="absolute w-full aspect-square bg-gradient-to-r from-yellow-300 to-red-600 rounded-full bottom-0" />
-                {/* <div className="absolute">
-                  <p className="text-4xl">Oleg Bratochkin</p>
-                  <p>Founder & CEO</p>
-                </div> */}
+                <div className="absolute w-full aspect-square bg-gradient-to-r from-yellow-300 to-red-600 rounded-full top-0 translate-y-[17.25%]" />
               </div>
+            </div>
+            <div className="text-center mt-4 text-slate-800">
+              <p className="text-4xl font-bold mb-2">Oleg Bratochkin</p>
+              <p className="text-lg">Founder & CEO</p>
+            </div>
+            <div className="flex flex-col justify-items-center gap-1 text-center text-slate-800 text-xs">
+              {bratochkinSocials.map((social) => (
+                <a
+                  href={social.href}
+                  key={social.href}
+                  className="flex items-center justify-center w-[315px] m-auto bg-white p-2 mt-3 rounded-full border border-gray-300 transition hover:border-red-500"
+                >
+                  <img className="h-[24px] w-[24px]" src={social.logo} alt="" />
+                  <p className="ml-2">{social.text}</p>
+                </a>
+              ))}
             </div>
           </div>
         </div>
@@ -129,10 +171,12 @@ export const Contacts = () => {
         >
           {locations.map((location, index) => (
             <motion.div key={index} variants={item}>
-              <p className="text-xl font-bold">
+              <p className="text-xl font-bold text-slate-800">
                 {location.icon} {location.country}
               </p>
-              <p className="text-lg font-light">{location.address}</p>
+              <p className="text-lg font-light text-slate-800">
+                {location.address}
+              </p>
             </motion.div>
           ))}
         </motion.div>
